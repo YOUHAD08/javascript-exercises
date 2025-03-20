baf54e046d74e67819cec6057f1ff74892caa6e1
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-03-20 20:56:12 +0000

    Solve the exercises in 03-REVERSESTRING

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let reverseCentence='';
+    for (let index = 0; index < sentence.length; index++) {
+        reverseCentence += sentence[sentence.length-index-1];
+        
+    }
+    return reverseCentence;
 };
 
 // Do not edit below this line
