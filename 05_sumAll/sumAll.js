69ff8861851bc49f93cab400d6eba7beb1c4451a
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-03-20 22:42:56 +0000

    Solve sumAll exercice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
-const sumAll = function() {
+const sumAll = function(a, b ) {
+    let result = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a<0 || b<0 ) return "ERROR";
+    if (a>b) [a, b] = [b, a];
+    for (let index = a; index < b+1; index++) result += index;
+    return result;
 
 };
 
