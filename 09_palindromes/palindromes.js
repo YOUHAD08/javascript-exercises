50e27c708c2daf4780307e76d0f44986caf09397
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-09-17 21:25:49 +0100

    Implement palindromes function and pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+  const cleaned = sentence.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  const reversed = cleaned.split("").reverse().join("");
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
