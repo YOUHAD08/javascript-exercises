1c0f6c22813ea78b76b0c201537f6cdcb04daf66
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-09-17 22:11:30 +0100

    Implement findTheOldest function and pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    return personAge > oldestAge ? person : oldest;
+  }, people[0]);
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
